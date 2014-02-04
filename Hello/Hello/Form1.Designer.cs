55474ce40bc3f4e96bde32c4c00165d003c776da
ozaharia <octavian.zaharia@bitsoftware.ro>
2014-02-04 17:47:29 +0200

    change 3

--- Hello/Hello/Form1.Designer.cs
+++ Hello/Hello/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 193);
+            this.button1.Location = new System.Drawing.Point(89, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 0;
